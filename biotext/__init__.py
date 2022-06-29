2e228a6b762c38292a7355dd3d189403b7ee051f
Diogo de Jesus Soares Machado <41521166+diogomachado-bioinfo@users.noreply.github.com>
2022-06-29 20:08:29 -0300

    Add files via upload

---
+++ biotext/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from . import fastatools
+from . import treetools
+from . import dnabits
+from . import aminocode
+from . import pubmedsearch
+from . import utilities
+from . import nltools
