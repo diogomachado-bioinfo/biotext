754ba96821823b6c4f107e11953edf981a2e1f32
Diogo de Jesus Soares Machado <41521166+diogomachado-bioinfo@users.noreply.github.com>
2023-09-17 23:52:13 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='biotext',
-      version='3.0.0.1',
+      version='3.0.1.0',
       author='Diogo de J. S. Machado',
       author_email='diogomachado.bioinfo@gmail.com',
       description='The biotext library offers resources for natural language processing based on bioinformatics tools',
@@ -9,7 +9,7 @@ setup(name='biotext',
       long_description_content_type='text/markdown',
       long_description=open('README.md').read(),
       zip_safe=False,
-      install_requires=['numpy', 'pandas', 'biopython', 'nltk', 'sweep', 'scipy', 'unidecode'],
+      install_requires=['numpy', 'pandas', 'biopython', 'sweep', 'scipy', 'unidecode'],
       license = 'BSD-3-Clause',
       url='https://github.com/diogomachado-bioinfo/biotext',
       )
