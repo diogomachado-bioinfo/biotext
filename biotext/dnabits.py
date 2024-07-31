29bb3fdc061cc3333628a43b2f8b4ec8ea98257e
Diogo de Jesus Soares Machado <41521166+diogomachado-bioinfo@users.noreply.github.com>
2024-07-30 22:30:16 -0300

    Update dnabits.py
    Optimization of functions.

--- biotext/dnabits.py
+++ biotext/dnabits.py
@@ -12,56 +12,50 @@ Functions:
 - bin_to_str: Converts a binary representation to a string.
 
 Author: Diogo de J. S. Machado
-Date: 13/07/2023
+Date: 30/07/2024
 """
-import re
-import numpy as np
 
 def encode_string(input_string):
     """
     Encodes a string with DNAbits.
-    
+
     Parameters
     ----------
     input_string : string
         Natural language text string to be encoded.
-        
+
     Returns
     -------
     encoded_string : string
         Encoded text.
-        
+
     Example
     -------
     Encode a string.
 
-    >>> import biotext as bt
     >>> input_string = "Hello world!"
-    >>> encoded_string = bt.dnabits.encode_string(input_string)
+    >>> encoded_string = encode_string(input_string)
     >>> print(encoded_string)
     AGACCCGCATGCATGCTTGCAAGATCTCTTGCGATCATGCACGCCAGA
     """
     
-    input_string=str_to_bin(input_string)
-    text_0 = re.findall('.',input_string[0:len(input_string)-1:2])
-    text_1 = re.findall('.',input_string[1:len(input_string):2])
-    text = np.transpose(np.array([(text_0),(text_1)]))
-    text = text.astype(int)
-    text = text * [1,2]
-    text = np.sum(text,1)
-    text = text.astype(str)
-    text[text=='0']='A'
-    text[text=='1']='C'
-    text[text=='2']='G'
-    text[text=='3']='T'
-    text=''.join(text)
-    encoded_string = text
+    input_string = str_to_bin(input_string)
+    
+    # Split the binary string into two parts
+    text_0 = input_string[0::2]
+    text_1 = input_string[1::2]
+    
+    # Convert binary pairs to DNA characters
+    binary_pairs = zip(text_0, text_1)
+    dna_chars = {'00': 'A', '01': 'G', '10': 'C', '11': 'T'}
+    encoded_string = ''.join(dna_chars[b0 + b1] for b0, b1 in binary_pairs)
+    
     return encoded_string
 
 def decode_string(input_string):
     """
-    Decodes a string with DNAbits reverse.	
-    
+    Decodes a string with DNAbits reverse.
+
     Parameters
     ----------
     input_string : string
@@ -71,28 +65,27 @@ def decode_string(input_string):
     -------
     decoded_string : string
         Decoded text.
-        
+
     Example
     -------
     Decode a string.
 
-    >>> import biotext as bt
     >>> encoded_string = "AGACCCGCATGCATGCTTGCAAGATCTCTTGCGATCATGCACGCCAGA"
-    >>> decoded_string = bt.dnabits.decode_string(encoded_string)
+    >>> decoded_string = decode_string(encoded_string)
     >>> print(decoded_string)
     Hello world!
     """
     
-    text = np.zeros((len(input_string),2)).astype(int)
-    dna=np.array(re.findall('.',input_string))
-    text[dna=='A']=[0,0]
-    text[dna=='C']=[1,0]
-    text[dna=='G']=[0,1]
-    text[dna=='T']=[1,1]
-    text=text.astype(str)
-    text=''.join(np.concatenate(text))
-    text=bin_to_str(text)
-    decoded_string = text
+    # Define the DNA to binary mapping
+    dna_to_bin = {'A': '00', 'G': '01', 'C': '10', 'T': '11'}
+    
+    # Convert DNA string to binary string
+    binary_string = ''.join(dna_to_bin[char] for char in input_string)
+    
+    # Convert binary string to ASCII string
+    decoded_string = ''.join(bin_to_str(binary_string[i:i+8])
+                             for i in range(0, len(binary_string), 8))
+    
     return decoded_string
 
 def encode_list(string_list, verbose=False):
@@ -181,19 +174,13 @@ def decode_list(input_list,output_file=None,verbose=False):
     return decoded_list
 
 def str_to_bin(string):
-    res = ''
-    for char in string:
-        tmp = bin(ord(char))[2:]
-        tmp = '%08d' %int(tmp)
-        tmp=tmp[::-1]
-        res += tmp
-    return res
+    return ''.join(f'{ord(char):08b}'[::-1] for char in string)
 
 def bin_to_str(string):
     res = ''
     for idx in range(int(len(string)/8)):
         cstr = string[idx*8:(idx+1)*8]
-        cstr=cstr[::-1] #???
+        cstr=cstr[::-1]
         tmp = chr(int(cstr, 2))
         res += tmp
     return res
